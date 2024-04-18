49b97bd7d8249a5133e454898e541f9f1a91013e
TuNombreDeUsuario <marcolopezg26@gmail.com>
2024-04-18 13:46:54 +0200

    New Favicons

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,10 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Bubble Marketplace</title>
                 <meta name="description" content="Next.js Dapp" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </Head>
             <MoralisProvider initializeOnMount={false}/*We are not using the Moralis server*/ >
                 <NotificationProvider>
