e5fa41dd14e0e0749a3394c581df00dfde584d3b
TuNombreDeUsuario <marcolopezg26@gmail.com>
2024-04-24 11:48:23 +0200

    Update to Mainnet Deployment

--- constants/ContractAddresses.js
+++ constants/ContractAddresses.js
@@ -1,3 +1,3 @@
 //Últimos contratos (definitivos excepto precio):
-export const marketplaceAddress = "0x1eDC1FD87287DA8832080AF9D94C34B79aB5D21B"
-export const nftAddress = "0x112d771B2A7f158dBE4B8Ce53e4Ac73036b211e0"
+export const marketplaceAddress = "0x2404486441481Acc6f96e8c647EB77D08e17cC20"
+export const nftAddress = "0x4F6F7E97e1dFea19663c4009B43E9d7DdcA41edA"
